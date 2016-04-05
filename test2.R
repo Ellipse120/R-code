b0e417666b3b1823f8dde5f52cc9c55d84c1cad3
James Lu <ellipse120@hotmail.com>
2016-04-05 22:21:39 +0800

    learn R's grammer...first commit.

---
+++ test2.R
@@ -0,0 +1,11 @@
+#About object type
+x = pi*10^2
+class(x) #x's class
+typeof(x) #x's type
+class(cars) #cars is integrated in R
+typeof(cars)
+names(cars) #cars's variable name
+summary(cars) #cars's summary
+head(class) #equal cars[1:6,]
+tail(class) 
+str(cars)
